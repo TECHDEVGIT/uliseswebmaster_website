1af4edf1d96612fd9cc0dc7b950c205a8382ab7c
TECHDEVGIT <ujhg.uw2017@gmail.com>
2020-10-25 11:45:30 -0300

    Sitio Web Version 1.0.0
    Ultima actualizacion del proyecto de pagina web para la Firma en Venezuela

---
+++ DIST/js/paginas/contacto.js
@@ -0,0 +1,13 @@
+
+/**
+ * INICIO DEL DOCUMENT READY
+ */
+$(document).ready(function() {
+
+	console.log( "La página ha cargado por completo..." );
+	$('[data-toggle="tooltip"]').tooltip();
+
+});
+/**
+ * FIN DEL DOCUMENT READY
+ */
